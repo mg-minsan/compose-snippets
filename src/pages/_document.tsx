446d520dc6821428bf3021d039d8db41a53e5c5a
mg-minsan <minsan.work@gmail.com>
2023-03-19 19:18:20 +0800

    Add responsiveness to the website [#1]

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en" data-theme="dracula">
-      <Head />
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no,viewport-fit=cover"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
